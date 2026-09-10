b6da350c65cc4850611e86c2d281cca2333624ba
kunalmeher27 <kunalmeher243@gmail.com>
2026-09-10 09:21:50 +0530

    Correcting styles

--- dr-frontend/src/pages/Analysis.jsx
+++ dr-frontend/src/pages/Analysis.jsx
@@ -3,7 +3,7 @@ import {
   useRef,
   useState,
 } from "react";
-import "../styles/liquidGlass.css";
+import "../styles/LiquidGlass.css";
 import useAnalysisStore from "../store/useAnalysisStore";
 import ResultDashboard from "../components/ResultDashboard";
 import GradCAMViewer from "../components/GradCAMViewer";
